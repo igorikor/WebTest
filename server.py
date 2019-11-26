43a1db241c0f44e1749a20861a9f5b37b9afc57b
igorikor <i.shirokih@mail.ru>
2019-11-26 18:32:08 +0500

    Ready to begginer work with arduino

--- server.py
+++ server.py
@@ -2,21 +2,21 @@ from flask import Flask,request,make_response
 
 app = Flask(__name__)
 com = ''
+command = ''
 
 status = 0
 
-#Запрос на подключение
 @app.route('/connect', methods=['POST','GET'])
 def get_com():
 	if request.method == 'POST':
 		global com
 		com = request.form['com']
 	if request.method == 'GET':
+		print(com)
 		return com
 		com = ''
 	return ("")
 
-#Статус ардуинки
 @app.route('/status', methods=['POST','GET'])
 def set_status():
 	if request.method == 'POST':
@@ -25,5 +25,15 @@ def set_status():
 	if request.method == 'GET':
 		return status
 	return("")
+
+@app.route('/command', methods=['POST','GET'])
+def get_command():
+	if request.method == 'POST':
+		global command
+		command = request.form('command')
+	elif request.method == 'GET':
+		if command != '':
+			return command
+			command = ''
 
 app.run(debug=True)
